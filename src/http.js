3f9fe51049bebc82d4930fe5f9d3eb8a20d5cfa3
Muhmmad Usman <hellmughal123@gmail.com>
2023-08-22 23:43:22 +0500

    improved

--- src/http.js
+++ src/http.js
@@ -2,7 +2,6 @@ import axios from "axios";
 function checkEnvVariable(variableName) {
 if (typeof process.env["REACT_APP_API_URL"] === 'undefined') {
   alert(`Environment variable '${variableName}' is not defined.`)
-  throw new Error(`Environment variable '${variableName}' is not defined.`);
 }}
 checkEnvVariable()
 export default axios.create({
